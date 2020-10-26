0fc0a0f722732c4c83fd57e2a668041a8955f9b1
dazzlingy1212 <kenny991x@gmail.com>
2020-10-27 00:08:23 +0800

    韋宏訂單新增

--- MotaiProject/Controllers/CustomerController.cs
+++ MotaiProject/Controllers/CustomerController.cs
@@ -334,5 +334,44 @@ namespace MotaiProject.Controllers
                 return false;
             }            
         }
+
+        //韋宏訂單
+        public ActionResult 詳細訂單(int id)
+        {
+            MotaiDataEntities db = new MotaiDataEntities();
+            var orderDetails = db.tOrderDetails.Where(o => o.oOrderId == id);
+            List<OrderDetailViewModel> orderdetail = new List<OrderDetailViewModel>();
+            List<OrderViewModel> OrderList = new List<OrderViewModel>();
+            if (orderdetail.FirstOrDefault() != null)
+            {
+                return View(OrderList);
+
+            }
+            return View("首頁");
+        }
+
+
+
+        //客戶訂單
+        public ActionResult 客戶訂單()
+        {
+            if (Session[CSession關鍵字.SK_LOGINED_CUSTOMER] != null)
+            {
+                tCustomer cust = Session[CSession關鍵字.SK_LOGINED_CUSTOMER] as tCustomer;
+                MotaiDataEntities db = new MotaiDataEntities();
+                List<tOrder> orderlist = db.tOrders.Where(o => o.oCustomerId.Equals(cust.CustomerId)).ToList();
+                List<OrderViewModel> OrderList = new List<OrderViewModel>();
+                foreach (var items in orderlist)
+                {
+                    OrderViewModel order = new OrderViewModel();
+                    order.Order = items;
+                    OrderList.Add(order);
+                    db.SaveChanges();
+                }
+                return View(OrderList);
+            }
+            return RedirectToAction("首頁");
+
+        }
     }
 }
---
+++ MotaiProject/ViewModels/OrderViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MotaiProject.ViewModels
+{
+    public class OrderViewModel
+    {
+        MotaiDataEntities dbContext = new MotaiDataEntities();
+        private tOrder ord;
+        public tOrder Order
+        {
+            get
+            {
+                if (ord == null)
+                {
+                    ord = new tOrder();
+                }
+                return ord;
+            }
+            set => ord = value;
+        }
+
+        [DisplayName("訂單編號")]
+        public int OrderId { get { return this.Order.OrderId; } set { Order.OrderId = value; } }
+        [DisplayName("客戶編號")]
+        public int oCustomerId { get { return Convert.ToInt32(this.Order.oCustomerId); } set { Order.oCustomerId = value; } }
+        [DisplayName("訂單地址")]
+        public string oAddress { get { return this.Order.oAddress; } set { Order.oAddress = value; } }
+        [DisplayName("訂單時間")]
+        public DateTime oDate { get { return this.Order.oDate; } set { Order.oDate = value; } }
+        [DisplayName("交貨時間")]
+        public Nullable<System.DateTime> oDeliverDate { get { return this.Order.oDeliverDate; } set { Order.oDeliverDate = value; } }
+        [DisplayName("員工編號")]
+        public Nullable<int> oEmployeeId { get { return this.Order.oEmployeeId; } set { Order.oEmployeeId = value; } }
+        [DisplayName("審核")]
+        public string oCheck { get { return this.Order.oCheck; } set { Order.oCheck = value; } }
+        [DisplayName("審核時間")]
+        public Nullable<System.DateTime> oCheckDate { get { return this.Order.oCheckDate; } set { Order.oCheckDate = value; } }
+        [DisplayName("升遷編號")]
+        public Nullable<int> oPromotionId { get { return this.Order.oPromotionId; } set { Order.oPromotionId = value; } }
+
+    }
+}
