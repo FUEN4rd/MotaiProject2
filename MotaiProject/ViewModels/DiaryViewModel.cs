7e3a56c6203eff47f6a651bbd2aaa5753304f80d
Lai-Wei-Tsung <free860918@gmail.com>
2020-10-27 09:30:15 +0800

    t1

--- MotaiProject/ViewModels/DiaryViewModel.cs
+++ MotaiProject/ViewModels/DiaryViewModel.cs
@@ -24,13 +24,32 @@ namespace MotaiProject.ViewModels
                 tdiary = value;
             }
         }
+        private tEmployee employeegetid;
+        public tEmployee employeesetid
+        {
+            get
+            {
+                if (employeegetid == null)
+                {
+                    employeegetid = new tEmployee();
+                }
+                return employeegetid;
+            }
+            set
+            {
+                employeegetid = value;
+            }
+        }
         public int DiaryId { get { return this.Diary.DiaryId; } set { this.Diary.DiaryId = value; } }
         public int dEmployeeId { get { return this.Diary.dEmployeeId; } set { this.Diary.dEmployeeId = value; } }
         public DateTime dDate { get { return this.Diary.dDate; } set { this.Diary.dDate = value; } }
         public string dWeather { get { return this.Diary.dWeather; } set { this.Diary.dWeather = value; } }
         public string dDiaryNote { get { return this.Diary.dDiaryNote; } set { this.Diary.dDiaryNote = value; } }
         public int dWarehouseNameId { get { return this.Diary.dWarehouseNameId; } set { this.Diary.dWarehouseNameId = value; } }
-
+        public string eAccount { get { return this.employeesetid.eAccount; } set { employeesetid.eAccount = value; } }
         public IEnumerable<SelectListItem> WarehouseName { get; set; }
     }
+    
+
+
 }
