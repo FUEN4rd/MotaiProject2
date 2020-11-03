7968a53cbda3731c719268091af4e979869a574d
Lai-Wei-Tsung <free860918@gmail.com>
2020-11-03 13:50:35 +0800

    account.cshtml

---
+++ MotaiProject/Controllers/AccountantController.cs
@@ -0,0 +1,31 @@
+﻿using MotaiProject.Models;
+using MotaiProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MotaiProject.Controllers
+{
+    public class AccountantController : Controller
+    {
+        // GET: Accountant
+        public ActionResult Accountant首頁()
+        {
+            if (Session[CSession關鍵字.SK_LOGINED_EMPLOYEE] == null)
+            {
+                return RedirectToAction("員工登入", "Employee");
+            }
+            else
+            {
+                tEmployee emp = Session[CSession關鍵字.SK_LOGINED_EMPLOYEE] as tEmployee;
+                EmployeeViewModels employee = new EmployeeViewModels();
+                employee.EmployeeId = emp.EmployeeId;
+                employee.eName = emp.eName;
+                employee.eAccount = emp.eAccount;
+                return View(employee);
+            }
+        }
+    }
+}
